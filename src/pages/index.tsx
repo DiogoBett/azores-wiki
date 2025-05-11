9b19573b3277d2d2f46ba92a4f83a9b5c3a86af4
Diogo Bettencourt <diogobettencourt97@gmail.com>
2025-05-11 12:08:22 +0100

    Update: Map of the Azores

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,69 +11,25 @@ export default function Home(): JSX.Element {
         <h1>Welcome to the Azores Wiki</h1>
         <p>Click on an island to learn more about it!</p>
         <div className={styles.mapContainer}>
-        <img
-          src="img/mapAzores.png"
-          alt="Map of the Azores"
-          useMap="#azores-map"
-          className={styles.mapImage}
-        />
-        <map name="azores-map">
-          <area
-            shape="circle"
-            coords="50,50,30"
-            href="/azores-wiki/docs/islands/corvo"
-            alt="Corvo"
+          <img
+            src="img/mapAzores.png"
+            alt="Map of the Azores"
+            useMap="#azores-map"
+            className={styles.mapImage}
           />
-          <area
-            shape="circle"
-            coords="150,150,40"
-            href="/azores-wiki/docs/islands/flores"
-            alt="Flores"
-          />
-          <area
-            shape="circle"
-            coords="250,250,40"
-            href="/azores-wiki/docs/islands/faial"
-            alt="Faial"
-          />
-          <area
-            shape="circle"
-            coords="300,300,40"
-            href="/azores-wiki/docs/islands/pico"
-            alt="Pico"
-          />
-          <area
-            shape="rect"
-            coords="350,350,400,380"
-            href="/azores-wiki/docs/islands/sao-jorge"
-            alt="São Jorge"
-          />
-          <area
-            shape="circle"
-            coords="450,450,30"
-            href="/azores-wiki/docs/islands/graciosa"
-            alt="Graciosa"
-          />
-          <area
-            shape="circle"
-            coords="550,550,40"
-            href="/azores-wiki/docs/islands/terceira"
-            alt="Terceira"
-          />
-          <area
-            shape="rect"
-            coords="650,650,700,700"
-            href="/azores-wiki/docs/islands/sao-miguel"
-            alt="São Miguel"
-          />
-          <area
-            shape="circle"
-            coords="750,750,30"
-            href="/azores-wiki/docs/islands/santa-maria"
-            alt="Santa Maria"
-          />
-        </map>
-      </div>
+          <map name="azores-map">
+            <area shape="rect" coords="918,407,810,363" href="/azores-wiki/docs/islands/corvo" alt="São Miguel" />
+            <area shape="rect" coords="906,527,935,549" href="/azores-wiki/docs/islands/santa-maria" alt="Santa Maria" />
+            <area shape="rect" coords="599,199,652,235" href="/azores-wiki/docs/islands/terceira" alt="Terceira" />
+            <area shape="rect" coords="498,145,520,165" href="/azores-wiki/docs/islands/graciosa" alt="Graciosa" />
+            <area shape="rect" coords="390,231,427,258" href="/azores-wiki/docs/islands/faial" alt="Faial" />
+            <area shape="rect" coords="509,283,432,248" href="/azores-wiki/docs/islands/pico" alt="Pico" />
+            <area shape="rect" coords="43,69,68,103" href="/azores-wiki/docs/islands/flores" alt="Flores" />
+            <area shape="circle" coords="70,37,12" href="/azores-wiki/docs/islands/corvo" alt="Corvo" />
+            <area shape="rect" coords="462,203,509,242" href="/azores-wiki/docs/islands/sao-jorge" alt="São Jorge 1" />
+            <area shape="rect" coords="549,256,510,222" href="/azores-wiki/docs/islands/sao-jorge" alt="São Jorge 2" />
+          </map>
+        </div>
       </main>
     </Layout>
   );
